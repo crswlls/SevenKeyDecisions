7b18a5c62f280a07a116383aad3d87ef0ed52d22
Chris Williams <chris.williams@audiogum.com>
2016-09-10 21:00:15 +0100

    1. Xamarin.Forms, 2. PCLs

---
+++ SevenKeyDecisions/SevenKeyDecisionsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace SevenKeyDecisions
+{
+    public partial class SevenKeyDecisionsPage : ContentPage
+    {
+        public SevenKeyDecisionsPage ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
+
