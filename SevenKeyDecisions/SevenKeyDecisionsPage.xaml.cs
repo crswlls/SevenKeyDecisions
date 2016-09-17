5d36c91a4a01fffe01b30536dabeb7430d1fa25f
Chris Williams <chris.williams@audiogum.com>
2016-09-17 20:59:12 +0100

    List xaml without data template

--- SevenKeyDecisions/SevenKeyDecisionsPage.xaml.cs
+++ SevenKeyDecisions/SevenKeyDecisionsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System.Linq;
+using SevenKeyDecisions.Core;
+using Xamarin.Forms;
 
 namespace SevenKeyDecisions
 {
@@ -8,6 +10,14 @@ namespace SevenKeyDecisions
         {
             InitializeComponent ();
         }
+        
+        protected override async void OnAppearing ()
+        {
+            base.OnAppearing ();
+            
+            var photoJson = await new PhotosRequester().GetPhotoInfos().ConfigureAwait(false);
+            var photoInfos = new PhotosParser().ParsePhotos(photoJson);
+            Device.BeginInvokeOnMainThread(() => photos.ItemsSource = photoInfos);
+        }
     }
 }
-
