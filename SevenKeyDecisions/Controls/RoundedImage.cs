28f56bc68f291fc20e91ea117f00cd37290b369e
Chris Williams <chris.williams@audiogum.com>
2016-10-02 20:30:55 +0100

    Add iOS custom renderer for rounded images

---
+++ SevenKeyDecisions/Controls/RoundedImage.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace SevenKeyDecisions
+{
+	public class RoundedImage : Image
+	{
+	}
+}
---
+++ iOS/CustomRenderers/RoundedImageRenderer.cs
@@ -0,0 +1,37 @@
+﻿using SevenKeyDecisions;
+using SevenKeyDecisions.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(RoundedImage), typeof(RoundedImageRenderer))]
+namespace SevenKeyDecisions.iOS
+{
+	public class RoundedImageRenderer : ImageRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement != null || Element == null)
+				return;
+			
+			MakeRound();
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+			if (e.PropertyName == VisualElement.HeightProperty.PropertyName ||
+				e.PropertyName == VisualElement.WidthProperty.PropertyName)
+			{
+				MakeRound();
+			}
+		}
+
+		private void MakeRound()
+		{
+			Control.Layer.CornerRadius = (float)(Element.Height / 2);
+			Control.ClipsToBounds = true;
+		}
+	}
+}
